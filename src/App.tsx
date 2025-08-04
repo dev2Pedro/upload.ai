c3e1e70ff98582290e19a12cd122c4764dc3f6f3
pedroServfaz <pedro.servfaz@gmail.com>
2025-08-04 16:44:46 -0300

    feat: adiciona formulário de upload de vídeo, botão e prompt de transcrição na sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { SiTailwindcss, SiPrisma, SiShadcnui } from "react-icons/si";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Textarea } from "./components/ui/textarea";
+import { FileVideo, Upload } from "lucide-react";
+import { Label } from "./components/ui/label";
 
 export function App() {
   return (
@@ -53,7 +55,42 @@ export function App() {
             selecionado.
           </p>
         </div>
-        <aside className="w-80"></aside>
+        <aside className="w-80 space-y-6">
+          <form className="space-y-6">
+            <label
+              htmlFor="video"
+              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+            >
+              <FileVideo className="w-4 h-4" />
+              Selecione um video
+            </label>
+
+            <input
+              type="file"
+              id="video"
+              accept="video/mp4"
+              className="sr-only"
+            />
+
+            <Separator />
+
+            <div className="space-y-1">
+              <Label htmlFor="transcription_prompt">
+                Prompt de transcrição
+              </Label>
+              <Textarea
+                id="transcription_prompt"
+                className="min-h-20 leading-relaxed"
+                placeholder="Inclua palavras-chave mencionadas no video, separadas por vírgula (,)"
+              />
+            </div>
+
+            <Button type="submit" className="w-full">
+              Carregar video
+              <Upload className="w-4 h-4 ml-2" />
+            </Button>
+          </form>
+        </aside>
       </main>
     </div>
   );
