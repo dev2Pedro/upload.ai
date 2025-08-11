796b4f5dba09073437a953235313147b44552e2b
pedroServfaz <pedro.servfaz@gmail.com>
2025-08-11 17:21:01 -0300

    feat(api): Implementa a comunicação com o backend via Axios

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -12,6 +12,7 @@ import {
 } from "react";
 import { getFFmpeg } from "@/lib/ffmpeg";
 import { fetchFile } from "@ffmpeg/util";
+import { api } from "@/lib/axios";
 
 export function VideoInputForm() {
   const [videoFile, setVideoFile] = useState<File | null>(null);
@@ -83,6 +84,20 @@ export function VideoInputForm() {
 
     const audioFile = await convertVideoToAudio(videoFile);
     console.log(audioFile, prompt);
+
+    const data = new FormData();
+
+    data.append("file", audioFile);
+
+    const response = await api.post("/videos", data);
+
+    const videoId = response.data.video.id;
+
+    await api.post(`/videos/${videoId}/transcription`, {
+      prompt,
+    });
+
+    console.log("Finalizou");
   }
 
   const previewURL = useMemo(() => {
